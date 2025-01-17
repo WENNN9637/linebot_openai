45f044939dd64717749625741bea71293ce094cc
WENNN9637 <153148198+WENNN9637@users.noreply.github.com>
2025-01-17 18:03:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import tempfile
 import datetime
 import time
 import string
-from finetuning import GPT_response
+#from finetuning import GPT_response
 
 app = Flask(__name__, template_folder='templates')
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
@@ -57,7 +57,7 @@ def handle_message(event):
     GPT_answer = GPT_response(msg)  # 在這裡呼叫 GPT_response 函數處理用戶的訊息
     print(GPT_answer)
     line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
-
+    
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))  # 默認為 5000，Render 部署時會指定 PORT
+    app.run(host="0.0.0.0", port=port)
