c32fe758986b75cdb4339c31f78023ac47c43599
WENNN9637 <153148198+WENNN9637@users.noreply.github.com>
2025-03-05 17:25:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 def GPT_response(text):
     # 接收回應
     response = openai.ChatCompletion.create(
-        model="gpt-4o", 
+        model="ft:gpt-4o-2024-08-06:personal::B5sbnkYa", 
         messages=[
             {"role": "user", "content": text},
         ],
